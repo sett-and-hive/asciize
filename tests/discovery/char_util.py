93ba0174dad57554161a444cdf0a1dc34437c10c
Tom Willis <tomwillis608@gmail.com>
2021-06-24 22:19:18 -0500

    extend tests and demonstrate 2nd xlation function

--- tests/discovery/char_util.py
+++ tests/discovery/char_util.py
@@ -1,5 +1,7 @@
 """char_util"""
-
+# TIL https://pypi.org/project/Unidecode/ but it is GPL2 and is mapping tables
+# http://semplicewebsites.com/sites/default/files/latinise_compact.js_.txt
+# 
 import unicodedata
 
 
@@ -9,14 +11,22 @@ def discovery(tests):
         print(f"Testing /{test}/")
         for c in test:
             uni = unicodedata.name(c)
-            print(f"/{c}/ {uni}")
+            dec = ord(c)
+            print(f"/{c}/ ({dec}) {uni}")
+            decomp = unicodedata.decomposition(c)
+            print(f"decomp /{c}/ {decomp}")
             nfkd_form = unicodedata.normalize("NFKD", c)
             for c1 in nfkd_form:
                 combo = unicodedata.combining(c1)
                 print(f"combo /{c1}/ {combo}")
                 decomp = unicodedata.decomposition(c1)
                 print(f"decomp /{c1}/ {decomp}")
-
+            # https://stackoverflow.com/questions/8935111/translating-letters-not-in-7bit-ascii-to-ascii-like-%C5%84-to-n-and-%C4%85-to-a
+            try:
+                cname = uni[:uni.index(' WITH')]
+                print (unicodedata.lookup(cname))
+            except (ValueError, KeyError):
+                print(f"Failed to find base letter of {uni}")
 
 if __name__ == "__main__":  # pragma: no cover
     # setup()
@@ -26,13 +36,15 @@ if __name__ == "__main__":  # pragma: no cover
         "ß",
         "ü",
         "ȕ",
-        "à",
-        "á",
-        "â",
+        # "à",
+        # "á",
+        # "â",
         "ã",
-        "ä",
-        "ó",
+        # "ä",
+        # "ó",
         "æ",
-        "å",
+        # where is "ue", "oe" liason?
+        # "å",
+        # other liason "ng" "hv"? "oi" 
     ]
     discovery(tests)
